8a9c6734d9d1ce9555a831641a6ae9016d2c2860
RadhShinde-spec <radhashinde810@gmail.com>
2025-06-23 15:47:45 +0530

    Updated AppApplication.java logic

--- src/main/java/com/rs/app/AppApplication.java
+++ src/main/java/com/rs/app/AppApplication.java
@@ -1,15 +1,15 @@
 package com.rs.app;
 
- 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
- 
-public class AppApplication {
- 
 
-    public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args);
-    }
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+		System.out.println("hello");
+	}
 }
+ 
